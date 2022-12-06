892dee1138b4760f3ada79ecd3b5e675647f89cc
Morning <morning@mstars-lab3-07s-iMac.local>
2022-12-06 11:49:27 +0800

    Function assignment one is done

---
+++ index.js
@@ -0,0 +1,52 @@
+//Simple assignment
+
+//Task-1
+
+let fullName = "Enkhmanlai"
+
+function full(name) {
+    return "HELLO " + name 
+}
+console.log(full(fullName))
+
+//Task-2
+
+let p = myName(`Enkhmanlai`, `Mnlght`)
+function myName (p1, p2) {
+    return (p1 + " " +p2);
+}
+console.log(p);
+
+//Task-3
+
+let x = myFunction(10, 15);   
+function myFunction(a, b) {
+  return a + b;             
+}
+console.log(x);
+
+//Task-4
+
+let q = myFunction(5, 3);   
+
+function myFunction(L, W) {
+  return L * W;             
+}
+console.log(`Area is equal to` + ` ` + q);
+
+//Task-5
+let w = my(20,30);
+function my (L,W) {
+    return L+L + W+W;
+}
+console.log(`Premeter is equal to` + ` ` + w);
+
+
+
+
+
+
+
+
+
+
